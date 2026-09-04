42222672d8aeabbe6f11e53224fd91324d4ab5f5
Peter McDonald <peterbehere@workspace.rool.app>
2026-09-04 03:20:54 +0000

    Update DetectiveDemo: web build + source (auto from Rool VM)

--- projects/DetectiveDemo/game/game/rooms/study/room_study.gd
+++ projects/DetectiveDemo/game/game/rooms/study/room_study.gd
@@ -7,7 +7,7 @@ var state: Data = load("res://game/rooms/study/room_study.tres")
 func _on_room_entered() -> void:
 	var detective := C.get_character("detective")
 	if detective:
-		detective.position = Vector2(900, 780)
+		detective.position = Vector2(1450, 900)
 		detective.face_left()
 	if not A.is_playing_cue("ambience_study"):
 		A.ambience_study.play()
